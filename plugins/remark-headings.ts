cbc6a3e148ecbdf57ce4b885fc35b23039c40232
Azat S. <to@azat.io>
2024-01-13 12:15:35 +0300

    feat: improve headings a11y

--- plugins/remark-headings.ts
+++ plugins/remark-headings.ts
@@ -17,7 +17,7 @@ export let remarkHeadings: Plugin<[], Root> =
         let { depth } = node as Heading
         let newNode = {
           value: `<h${depth} id="${id}">
-            <a href="#${id}" class="heading-anchor">
+            <a href="#${id}" aria-label="${text}" class="heading-anchor">
               <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M7 20L10 4M14 20L17 4M19.5 15H3.5M20.5 9L4.5 9" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
               </svg>
